2557e48e52b3817330d8595ecf582ffae946c487
Katya Weicht <katya.weicht@gmail.com>
2025-02-19 09:19:21 -0700

    initialize Passenger class and tests

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -7,5 +7,24 @@ end
 
 # Your tests should start here!
 RSpec.describe Passenger do 
-    
+    before(:each) do
+      @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+    end
+    it 'exists' do
+      expect(@charlie).to be_a(Passenger)
+      expect(@taylor).to be_a(Passenger)
+    end
+    it 'has a name' do
+      expect(@charlie.name).to eq("Charlie")
+      expect(@taylor.name).to eq("Taylor")
+    end
+    it 'has an age' do
+      expect(@charlie.age).to eq(18)
+      expect(@taylor.age).to eq(12)    
+    end
+    it 'returns true/false if adult' do
+      expect(@charlie.adult?).to eq(true)
+      expect(@taylor.adult?).to eq(false)
+    end
 end
