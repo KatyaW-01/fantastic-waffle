772f71776e17584f25d75bb867e7244d54910d94
Katya Weicht <katya.weicht@gmail.com>
2025-02-19 10:43:14 -0700

    Add vehicle and passenger methods

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,17 @@
 class Park
-    attr_reader :name, :admission_price
+    attr_reader :name, :admission_price, :vehicles, :passengers
     def initialize(name,admission_price)
         @name = name
         @admission_price = admission_price
         @vehicles = []
+        @passengers = []
+    end
+
+    def vehicles_enter_park(vehicle)
+        @vehicles << vehicle
+    end
+
+    def passengers_enter_park(passenger)
+        @passengers << passenger
     end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -26,4 +26,36 @@ RSpec.describe Park do
         @vehicle_4 = Vehicle.new("2009", "Toyota", "Prius")
         @vehicle_5 = Vehicle.new("1998", "Jeep", "Wrangler")
     end
+    it 'can return an array of vehicles that have entered the park' do
+        expect(@park.vehicles).to eq([])
+
+        @park.vehicles_enter_park(@vehicle_1)
+        @park.vehicles_enter_park(@vehicle_2)
+        @park.vehicles_enter_park(@vehicle_3)
+        @park.vehicles_enter_park(@vehicle_4)
+        @park.vehicles_enter_park(@vehicle_5)
+
+        expect(@park.vehicles).to eq([@vehicle_1,@vehicle_2,@vehicle_3,@vehicle_4,@vehicle_5])
+    end
+    before(:each) do
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        @samantha = Passenger.new({"name" => "Samantha", "age" => 19})
+        @becky = Passenger.new({"name" => "Becky", "age" => 53})
+        @emory = Passenger.new({"name" => "Emory", "age" => 27})
+    end
+    it 'can return an array of passengers that have entered the park' do
+        expect(@park.passengers).to eq([])
+
+        @park.passengers_enter_park(@charlie)
+        @park.passengers_enter_park(@jude)
+        @park.passengers_enter_park(@taylor)
+        @park.passengers_enter_park(@samantha)
+        @park.passengers_enter_park(@becky)
+        @park.passengers_enter_park(@emory)
+
+        expect(@park.passengers).to eq([@charlie,@jude,@taylor,@samantha,@becky,@emory])
+
+    end
 end
